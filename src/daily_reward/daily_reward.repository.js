c481233d7a57d45c4a76fc6089a5477e032a1812
owenunda <oweenunda.dev@gmail.com>
2025-12-27 12:31:07 -0500

    feat: implement daily reward recording in the database

--- src/daily_reward/daily_reward.repository.js
+++ src/daily_reward/daily_reward.repository.js
@@ -1,2 +1,16 @@
 import pool  from '../config/db.js';
 
+const rewardRecord = async (userId, rewardAmount, claimed_at) => {
+  try {
+    const { rows } = await pool.query(
+      'INSERT INTO daily_rewards (user_id, reward_amount, claimed_at) VALUES ($1, $2, $3) RETURNING *',
+      [userId, rewardAmount, claimed_at]
+    );
+    return rows[0];
+  } catch (error) {
+    throw new Error(`Error al guardar el registro de recompensa diaria: ${error.message}`);
+  }
+}
+export default {
+  rewardRecord
+}
--- src/daily_reward/daily_reward.service.js
+++ src/daily_reward/daily_reward.service.js
@@ -1,35 +1,46 @@
 import redisClient from "../redis/redis.client.js";
-
+import dailyRewardRepository from "./daily_reward.repository.js";
 
 const grantDailyReward = async (userId) => {
-  if (!userId) {
-    throw new Error("userId es requerido");
-  }
-  // clave única por usuario
-  const redisKey = `daily:cooldown:user:${userId}`;
-  // valida si la clave ya existe
-  const alreadyClaimed = await redisClient.exists(redisKey);
+  try {
+    if (!userId) {
+      throw new Error("userId es requerido");
+    }
+    // clave única por usuario
+    const redisKey = `daily:cooldown:user:${userId}`;
+    // valida si la clave ya existe
+    const alreadyClaimed = await redisClient.exists(redisKey);
 
-  // valida si existe
-  if (alreadyClaimed) {
-    const ttl = await redisClient.ttl(redisKey);
+    // valida si existe
+    if (alreadyClaimed) {
+      const ttl = await redisClient.ttl(redisKey);
 
+      return {
+        error: "Ya reclamaste tu recompensa diaria",
+        retry_in_seconds: ttl,
+      };
+    }
+
+    // si no existe, le damos la recompensa y seteamos la clave con expiración de 24 horas
+    await redisClient.set(redisKey, Date.now(), {
+      EX: 60 * 60 * 24, // 24 horas
+    });
+
+    // guardamos el registro en la base de datos
+    const rewardRecord = {
+      user_id: userId,
+      reward_amount: 100,
+      claimed_at: new Date(),
+    }
+    await dailyRewardRepository.rewardRecord(rewardRecord.user_id, rewardRecord.reward_amount, rewardRecord.claimed_at);
     return {
-      error: "Ya reclamaste tu recompensa diaria",
-      retry_in_seconds: ttl,
+      success: true,
+      reward: 100,
+      message: "Recompensa diaria reclamada 🎉",
     };
+  } catch (error) {
+    throw new Error(`Error al otorgar la recompensa diaria: ${error.message}`);
   }
-
-  // si no existe, le damos la recompensa y seteamos la clave con expiración de 24 horas
-  await redisClient.set(redisKey, Date.now(), {
-    EX: 60 * 60 * 24, // 24 horas
-  });
-
-  return {
-    success: true,
-    reward: 100,
-    message: "Recompensa diaria reclamada 🎉",
-  };
 }
 
 export default {
