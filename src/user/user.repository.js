055deb7985788c07ed02446bf7fed249ccc654b5
owenunda <oweenunda.dev@gmail.com>
2025-12-27 14:05:31 -0500

    feat: update user query to select specific fields in getUserById function

--- src/user/user.repository.js
+++ src/user/user.repository.js
@@ -3,7 +3,7 @@ import pool from '../config/db.js';
 
 const getUserById = async (userId) => {
   try {
-    const query = 'SELECT * FROM users WHERE id = $1';
+    const query = 'SELECT id, email, created_at, reward_points FROM users WHERE id = $1';
     const values = [userId];
     const result = await pool.query(query, values);
     return result.rows[0];
